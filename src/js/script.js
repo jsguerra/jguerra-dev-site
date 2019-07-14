116a7623b43ec9e88b45d9dc31dee9bfd9022ac0
Jose Guerra <jose@gcostudios.com>
2019-07-14 22:34:26 +0100

    added header color change script

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,31 @@
-console.log('hello world again')
+jQuery(function($) {
+
+  'use strict';
+
+  var HOME;
+
+  // ================================
+  // Home
+  // ================================
+
+  HOME = {
+
+    init: function() {
+      this.headerOpacity();
+    },
+
+    headerOpacity: function() {
+      $(window).on('scroll', function() {
+        if ($(document).scrollTop() > 150 && $(this).width() >= 600) {
+          $('#master-header').addClass('darken');
+        } else {
+          $('#master-header').removeClass('darken');
+        }
+      });
+    }
+
+  };
+  
+  HOME.init();
+
+}); //jQuery
